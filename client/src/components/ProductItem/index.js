733c244969f8b916b9c6c09dc63d7fe071502f7f
Sean Gordon <nodrog.naes@gmail.com>
2021-07-13 19:53:15 -0400

    productitem index

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,17 +3,22 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
 
 //TODO: remove the line below for 'useStoreContext. We will use React-Redux to generate state and dispatch method
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
+
 //TODO: import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
   // TODO: remove the line below. We will use useDispatch and useSelector to generate state and dispatch
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+
   // TODO: Create a const variable 'dispatch' and assign the returned value from useDispatch()
+  const dispatch = useDispatch();
   // TODO: create a const varailbe 'state' and assign the returned value from useSelector(...)
+  const state = useSelector(state => state);
   // Hint: search 28-Stu_Mini-Project for how to create the two variables, dispatch and state
 
   const { image, name, _id, price, quantity } = item;
