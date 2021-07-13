7c5ff75e6cfff0b4202c1449ce1d7c50c65fe7aa
Sean Gordon <nodrog.naes@gmail.com>
2021-07-13 19:41:15 -0400

    store js

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -2,3 +2,7 @@
 // import { createStore } from 'redux';
 // import reducers from './reducers';
 // Create a default export of `createStore` that accepts an argument of `reducers`.
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+export default createStore(reducers);
