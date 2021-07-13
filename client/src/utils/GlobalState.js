45209c9c9525332d1c75cc3a083fbfa7ca97d1ae
Sean Gordon <nodrog.naes@gmail.com>
2021-07-13 19:49:50 -0400

    globalstate commented out

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,24 +1,24 @@
 //TODO: this file is not being used after refactoring to Redux and should be removed from the folder
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+// import React, { createContext, useContext } from "react";
+// import { useProductReducer } from './reducers'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
 
-export { StoreProvider, useStoreContext };
+// export { StoreProvider, useStoreContext };
